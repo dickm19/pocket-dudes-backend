008cce80d665af83eb984894d3e0de21a9f61444
dickm19 <maryclairedick@gmail.com>
2020-12-12 14:46:00 -0500

    seeded database with test user and pets

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many: :pets
-    has_many: :toys
-    has_many: :food
+    has_many :pets
+    has_many :toys
+    has_many :food
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,59 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+user = User.create(name: 'Mary', username: 'maryc')
+
+###Bubbs
+blue_bubb = Pet.create(name: 'Blue Bubb', user_id: 1, age: 1, happiness: 10, hunger: 10, image_url: 'https://keep.google.com/u/0/media/v2/1u7MYU1o3chAw0skZ18gFdEM67JVR6_zdRaFuYKULp1yKev_iuCb3ZK7EK2P_jA/1laYoQf9WBGq4PLvYaXNeWtGcOmsKNFaZ40ClldEGgTorfaGuf8PXlGceTzWA?accept=image/gif,image/jpeg,image/jpg,image/png,image/webp,audio/aac&sz=1068' )
+
+green_bubb = Pet.create(name: 'Green Bubb', user_id: 1, age: 1, happiness: 10, hunger: 10, image_url: 'https://keep.google.com/u/0/media/v2/10RZ7FL5VAjEnoSh8-Hsnh_JQ3ssHurGEYO5SJ-IyKZEYohE-ga8igG1HXYP7/1tjXJK9q6SsAnX2SNkNZNY1C8axI7FBxfUFkF9SsZm0do4GTFrs_ClASALfNwWA?accept=image/gif,image/jpeg,image/jpg,image/png,image/webp,audio/aac&sz=990')
+
+pink_bubb = Pet.create(name: 'Pink Bubb', user_id: 1, age: 1, happiness: 10, hunger: 10, image_url: 'https://keep.google.com/u/0/media/v2/1kkLDyN2de9FB1eObfuwE2EyjH7UkDhpotuQM-B3EluISpnxA0_CiZQVeROO2IA/16nQ30RL4Mt8e3HmynxEctc3iJUaUDOmIiPwdC5gdyc0NoQtgIEyL05_Tl-VS?accept=image/gif,image/jpeg,image/jpg,image/png,image/webp,audio/aac&sz=1036')
+
+yellow_bubb = Pet.create(name: 'Yellow Bubb', user_id: 1, age: 1, happiness: 10, hunger: 10, image_url: 'https://keep.google.com/u/0/media/v2/1orkJVfEQLlcBVoNPI5B_jNFfZ5XN-3SzfiVPxoT1FUt36bwUAteqpyZC4czaQw/16QG4lYSk7kXlNDYJfgp2_rjRy07n8LB_mgtMj3cDHcN_HYDE133rgf_DrPnt?accept=image/gif,image/jpeg,image/jpg,image/png,image/webp,audio/aac&sz=1028' )
+
+red_bubb = Pet.create(name: 'Red Bubb', user_id: 1, age: 1, happiness: 10, hunger: 10, image_url: 'https://keep.google.com/u/0/media/v2/1Pdo7dtCB2p7WmRSdI09jet-pk0QASMxMQLj4L0-HgSEZoQvH1EsEQB5ccTM/1lH3cdeMc969c9LVY05VXACjOJWUUwaCTcY-bUDzp6vQfsfawjKieGYyK4aVGDw?accept=image/gif,image/jpeg,image/jpg,image/png,image/webp,audio/aac&sz=1048' )
+
+
+###Morps
+blue_morp = Pet.create(name: 'Blue Morp', user_id: 1, age: 1, happiness: 10, hunger: 10, image_url: 'https://keep.google.com/u/0/media/v2/1yfmxUdq5UXMw6yUWvDGvtzP6NS-h3Zosb_Pk58ftRPxTxwAQjnX8xAZ2E6tSDQ/1HF6mQ4lKCyrhcJucCVebQOhcYQDS8k8aQ7bnwROW7Hll6w1RbnT_G9rOhhF4Lw?accept=image/gif,image/jpeg,image/jpg,image/png,image/webp,audio/aac&sz=1026' )
+
+
+
+
+green_morp = Pet.create(name: 'Green Morp', user_id: 1, age: 1, happiness: 10, hunger: 10, image_url: 'https://keep.google.com/u/0/media/v2/1ic12R3J88TjzwItWgzxZ9K1GXB3hE4Zon7mv5SSnBHKxLYjGEuIx7-T_8zHiAw/1DtHoJK5uWFHsLPvV5k5AknEYfJPqoCUy_CR1_XiTHpPbatU7NN0Bi2Qi_2r9fA?accept=image/gif,image/jpeg,image/jpg,image/png,image/webp,audio/aac&sz=1000' )
+
+yellow_morp = Pet.create(name: 'Yellow Morp', user_id: 1, age: 1, happiness: 10, hunger: 10, image_url: 'https://keep.google.com/u/0/media/v2/18iHhgBTBbrvCj_1Bta1UmMXtmvGrLBCFpuKyKH-fP06awlyKA8Vb7AyeBe0n/1KbmXgq0ARD04y28wwhlZWwc_h4uk7OB_Ji2EHn_Wy_IPdI_A_4TFinZ4nOW8QQ?accept=image/gif,image/jpeg,image/jpg,image/png,image/webp,audio/aac&sz=1018')
+
+
+pink_morp = Pet.create(name: 'Pink Morp', user_id: 1, age: 1, happiness: 10, hunger: 10, image_url: 'https://keep.google.com/u/0/media/v2/15LxzfSbG_MlckMfF1sJnkovzJShY1THh3eystpxNaEaGeuGWOWvrcdOpCoaPdg/1ksuoZgeIleLqB9uSYD-CU-dOBFkTBfYQs4JyiHtzFKzgDGjn7pNC8N1NRZMA?accept=image/gif,image/jpeg,image/jpg,image/png,image/webp,audio/aac&sz=1068')
+
+red_morp = Pet.create(name: 'Red Morp', user_id: 1, age: 1, happiness: 10, hunger: 10, image_url: 'https://keep.google.com/u/0/media/v2/12uYi6h3oFrkTun0enpI551FVHMwkW97DdYn4IKFEZrlc4zfNbxFLojWYlDqNWg/1yFYP0JAL46c9i3A9LjkzG537urQUvemJ2w0gEXQo0AkybNha3JbThIzSmYB5?accept=image/gif,image/jpeg,image/jpg,image/png,image/webp,audio/aac&sz=1010')
+
+###Florps
+blue_florp = Pet.create(name: 'Blue Florp', user_id: 1, age: 1, happiness: 10, hunger: 10, image_url: 'https://keep.google.com/u/0/media/v2/1LQ7Za-IodMtc68ri64USYwOMjdVkKA4DxORttmeU8VKruY9JgmmafyMcoe-AJQ/1pgmmGX7BKDxJ4HswCkc1Hp6p3SSem1JjEc1tm4hTQiYRReeiN4DiyUXmSyk-Pg?accept=image/gif,image/jpeg,image/jpg,image/png,image/webp,audio/aac&sz=662')
+
+
+green_florp = Pet.create(name: 'Green Florp', user_id: 1, age: 1, happiness: 10, hunger: 10, image_url: 'https://keep.google.com/u/0/media/v2/1ni5PsqWclUY3F4sbpTqew-8rK8KEfJyVBYG19LxYgNGA5bXWUEsqeiO9CkoX/1i1sYSSTY1bTCgYv_47gQdFytcq6G5vsVqBCkc4-JuWHLqS7l85PNTzu2fp2W?accept=image/gif,image/jpeg,image/jpg,image/png,image/webp,audio/aac&sz=688' )
+
+yellow_florp = Pet.create(name: 'Yellow Florp', user_id: 1, age: 1, happiness: 10, hunger: 10, image_url: 'https://keep.google.com/u/0/media/v2/1QCPO_6OT6MVxBgxmW4EH5O0K4iwsGNMM-cYyCv_Atq7I1qVo013t7rfxG4Kn/1SUh2wgrLR0G83fyFS5_-Ibm6R149R4krvb0vWAjBxzjWDNDTRg1CZN0G3qkd?accept=image/gif,image/jpeg,image/jpg,image/png,image/webp,audio/aac&sz=642' )
+
+
+pink_florp = Pet.create(name: 'Pink Florp', user_id: 1, age: 1, happiness: 10, hunger: 10, image_url: 'https://keep.google.com/u/0/media/v2/1BF5pjeM_7Oj62nhyaT64-Jh3p5DXvqK4JbSTBQSlyuAlGexD1pEqvEFPpntg8g/1OYeoD-sBcK09EBBakWyqupyiwQc0R6oZ9C02VzXv84H1_fRHHzvISFYpz7QH?accept=image/gif,image/jpeg,image/jpg,image/png,image/webp,audio/aac&sz=646' )
+
+red_florp = Pet.create(name: 'Red Florp', user_id: 1, age: 1, happiness: 10, hunger: 10, image_url: 'https://keep.google.com/u/0/media/v2/1R3aWjGeOta7ifWpoFpf7Qe3msBC8g_lFCaicoUeTYTTe9XIKm_E2LEraFSBNyw/1-hyFkqXQ2GPL7kDIosQW0wg7X1ZrUhcSrssVuHXywAsy86HGI4bjh6b1uJKz?accept=image/gif,image/jpeg,image/jpg,image/png,image/webp,audio/aac&sz=642' )
+
+
+###Beepos
+blue_beepo = Pet.create(name: 'Blue Beepo', user_id: 1, age: 1, happiness: 10, hunger: 10, image_url: 'https://keep.google.com/u/0/media/v2/1K74bQf3w_CP1l6QppeNtFIw8PgsZeEDKMil3CnYMfNKBsHqklrkNQS4-a1wsIA/1wED5X2t4XYdNRBEDFGW8keWerL4Lb9VTAZJDPtHF0Z-7bQ2dzKol9GnE3cEubA?accept=image/gif,image/jpeg,image/jpg,image/png,image/webp,audio/aac&sz=712' )
+
+
+
+
+green_beepo = Pet.create(name: 'Green Beepo', user_id: 1, age: 1, happiness: 10, hunger: 10, image_url: 'https://keep.google.com/u/0/media/v2/1Dj4P6S76ZKsudVbrBkrry_5kBCn-4dKs6z22or2kckU-DhtpdGlljLMTtcbq/1jKdOf77YiVYhHzmPA55vp7r4X5iU1j8-s8cb0UZJDf_t6cuk-oOV_IJeFxc?accept=image/gif,image/jpeg,image/jpg,image/png,image/webp,audio/aac&sz=706' )
+
+yellow_beepo = Pet.create(name: 'Yellow Beepo', user_id: 1, age: 1, happiness: 10, hunger: 10, image_url: 'https://keep.google.com/u/0/media/v2/1i45ArMiTXfjQXkd_2wjS3cE-RZ1vpildnaBF0ID54OTcdU1OkRW-orVUE3x5/1KFZzfMvUzPUrze3y7KfXrv2kwxkOVEDQRnMsoTV2g1Fbaigwke1cTGanm7FSJA?accept=image/gif,image/jpeg,image/jpg,image/png,image/webp,audio/aac&sz=702' )
+
+
+pink_beepo = Pet.create(name: 'Pink Beepo', user_id: 1, age: 1, happiness: 10, hunger: 10, image_url: 'https://keep.google.com/u/0/media/v2/1MjQu6H9zFzICy8wHyF2V5khybGGh1fmL7rKmP11mjiiD_4z4lM3AAdKN510Zfw/1V0pY2BOxhvPsunP5Bne2iKIqpqUWv8aUuCdb6XlC13uO694Z9nK18k3cacOINQ?accept=image/gif,image/jpeg,image/jpg,image/png,image/webp,audio/aac&sz=708' )
+
+red_beepo = Pet.create(name: 'Red Beepo', user_id: 1, age: 1, happiness: 10, hunger: 10, image_url: 'https://keep.google.com/u/0/media/v2/1lrikPDUxLaO7u89_bPe-pnqoUBIzIVhamEJljDzk8zeI8pZ5m2HpD-e7M3NG/171tQ3MJg05O85QIlvPfbIii63HtCW29jk6Jwj6VfmeZZvr26YhSSngtDvsV4?accept=image/gif,image/jpeg,image/jpg,image/png,image/webp,audio/aac&sz=740' )
