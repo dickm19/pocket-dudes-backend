ae8fa5ac28a8ff8b515a01a78c56b4cf4887c544
dickm19 <maryclairedick@gmail.com>
2020-12-14 10:11:21 -0500

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users only: [:create, :index, :update, :distroy]
+  resources :pets only: [:create, :index, :update, :distroy]
+  resources :items only: [:index]
 end
